012a8cc6e943c771dd85c308a7cf820a8d080328
Luis Tripa <54379665+luistripa@users.noreply.github.com>
2020-05-04 14:06:42 +0100

    Updated TestParser

--- TestParser.py
+++ TestParser.py
@@ -1,17 +1,25 @@
 from os import system
 
 TEST_LIST = [
+	"base_add",
+	"base_upload",
+	"base_share",
+	"minspace",
+	"pre_add",
+	"pre_upload",
+	"pre_share",
+	"pre_listfiles"
 ]
 
-NUMBER_OF_TESTS = 15
+NUMBER_OF_TESTS = len(TEST_LIST)
 
-system("cd src/ ; javac -Xlint -d ../bin/production/FakeBook Main.java ; cd ..")
+system("cd src/ ; javac -Xlint -d ../out/production/FakeBook Main.java ; cd ..")
 
-COMMAND_RUN = "cd bin ; java Main < ../WeKeepSecretsTests/%.2d_in_%s.txt > ../tests/%.2d_%s_test_out.txt"
+COMMAND_RUN = "cd out/production/FakeBook ; java Main < ../../../FakeBookTests/%d_in_%s.txt > ../../../tests/%d_%s_test_out.txt"
 
 for i in range(0, NUMBER_OF_TESTS):
 	system(COMMAND_RUN % (i+1, TEST_LIST[i], i+1, TEST_LIST[i]))
-	code = system("diff tests/%.2d_%s_test_out.txt WeKeepSecretsTests/%.2d_out_%s.txt" % (i+1, TEST_LIST[i], i+1, TEST_LIST[i]))
+	code = system("diff tests/%d_%s_test_out.txt FakeBookTests/%d_out_%s.txt" % (i+1, TEST_LIST[i], i+1, TEST_LIST[i]))
 	if code != 0:
 		print("------------------------")
 		print("       FAILED %.2s        " % str(i+1))
